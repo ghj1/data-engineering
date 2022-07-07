87650e81f94d1adb3b48f5924d8e7154ef2c4530
ghj1 <ghj12222@gmail.com>
2022-07-07 15:59:02 +0900

    cm

--- dataprep/parquet-to-csv.py
+++ dataprep/parquet-to-csv.py
@@ -1,3 +1,4 @@
 import pandas as pd
-df = pd.read_parquet('fhvhv_tripdata_2020-03.parquet')
-df.to_csv('fhvhv_tripdata_2020-03.csv')
+import sys
+df = pd.read_parquet('yellow_tripdata_2021-07.parquet')
+df.to_csv('yellow_tripdata_2021-07.csv')
